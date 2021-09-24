e2a38e7686c1f383373017ba532ee1ff637e9c86
jason-afresh <jasonhuels@afreshtechnologies.com>
2021-09-23 22:18:45 -0700

    fix js string math

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
 
 function getDeliveries() {
     let response = 'Oops something went wrong';
-    let deliveryDay = document.getElementById('delivery').value || -1;
-    let lag = document.getElementById('lag').value || -1;
+    let deliveryDay = parseInt(document.getElementById('delivery').value) || -1;
+    let lag = parseInt(document.getElementById('lag').value) || -1;
     document.getElementById('delivery').value = '';
     document.getElementById('lag').value = '';
+    console.log(deliveryDay)
+    console.log(lag)
     if(deliveryDay < 7 && deliveryDay >= 0 && lag > 0) {
         let delivery = days[deliveryDay];
         let order;
@@ -14,11 +16,10 @@ function getDeliveries() {
         }
         if(deliveryDay - lag >= 0) {
             order = days[deliveryDay-lag];
-        } else if(lag % 7 === 0) {
-            order = days[deliveryDay];
         } else {
             order = days[deliveryDay+7-lag]
         }
+        console.log(order)
         response = `Order DOW = ${order} delivery DOW = ${delivery}`
     }
     
