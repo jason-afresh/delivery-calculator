abffb88791126f849b5b6be732820302f3f4c823
jason-afresh <jasonhuels@afreshtechnologies.com>
2021-09-23 22:22:53 -0700

    remove logs

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ function getDeliveries() {
     let lag = parseInt(document.getElementById('lag').value) || -1;
     document.getElementById('delivery').value = '';
     document.getElementById('lag').value = '';
-    console.log(deliveryDay)
-    console.log(lag)
     if(deliveryDay < 7 && deliveryDay >= 0 && lag > 0) {
         let delivery = days[deliveryDay];
         let order;
@@ -19,10 +17,9 @@ function getDeliveries() {
         } else {
             order = days[deliveryDay+7-lag]
         }
-        console.log(order)
+  
         response = `Order DOW = ${order} delivery DOW = ${delivery}`
-    }
-    
+    }  
     let result = document.getElementById('result');
     result.innerHTML = response
 }
